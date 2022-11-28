e2173debe63040454fd224f5101ad607b50077a3
al395603 <al395603@uji.es>
2022-11-28 18:30:59 +0100

    Arreglos

--- Assets/Scripts/Pathfinding/GridManager.cs
+++ Assets/Scripts/Pathfinding/GridManager.cs
@@ -78,12 +78,13 @@ public class GridManager<TGridObject>
         return cellSize;
     }
 
-    //Convierte x e y en posiciones del escenario   
+    //Convierte x e y de una celda en posiciones del escenario   
     public Vector3 GetWorldPosition(int x, int y){
 
         return new Vector3(x,y) * cellSize + originPosition;
     }
 
+    //Convierte posiciones del escenario en x e y de una celda
     public void GetXY(Vector3 worldPosition, out int x, out int y)
     {
         x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
